37134f9c34841f657c08236e9b36cb1353ad313b
Chiara Kappe <chiarakappe@Chiaras-MacBook-Pro-2.local>
2025-11-12 22:28:05 +0100

    restructured stuff

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: chiarakappe <chiarakappe@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/08 17:49:56 by chiarakappe       #+#    #+#             */
-/*   Updated: 2025/09/11 19:13:33 by chiarakappe      ###   ########.fr       */
+/*   Updated: 2025/11/12 22:27:02 by chiarakappe      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,20 @@
 
 int main(int ac, char **av)
 {
-	if (ac == 1)
+	if (ac < 2) {
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-	else
-	{
-		for (int i = 1; i < ac; ++i)
-		{
+		return 0;		
+	}
+
+	for (int i = 1; i < ac; ++i) {
 			for (int j = 0; av[i][j]; ++j)
 				std::cout << (const char)std::toupper(av[i][j]);
 			if (i + 1 < ac)
 				std::cout << " ";
-		}
-	}
+	} 
 	std::cout << std::endl;
 	return 0;
+
 }
 
 
